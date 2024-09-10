588f0cee42df2fc3a08eb54a3f63e8fcd34c29e8
Prirogativus <prirogativus@gmail.com>
2024-09-10 18:03:04 +0200

    Создал PH порта
    Создал Плейсхолдер порта плюс деньги в ГеймМенеджере

---
+++ G2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int money = 100;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
