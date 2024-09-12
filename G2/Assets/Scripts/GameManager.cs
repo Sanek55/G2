353dc47a963aa4a72610d62c66cde51b39981a4e
Sanek55 <cecggcecg@gmail.com>
2024-09-12 22:02:19 +0200

    добавления монеты и ее отображения на сцене
    (отображения на сцене еще не доработаное)

--- G2/Assets/Scripts/GameManager.cs
+++ G2/Assets/Scripts/GameManager.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // библиотека для использования 11 строки
+using TMPro; //библиотека которая позволяет испоьзовать TextMeshProUGUI
 
 public class GameManager : MonoBehaviour
 {
-    public int money = 100;
-    
+    public int money = 1000; // количество монет
+
+    public TextMeshProUGUI moneyText; 
+
+   
 
     void Start()
     {
-        
+      
     }
 
    
     void Update()
     {
-        
+        UpdateMoneyText();
+    }
+
+    public void UpdateMoneyText()
+    {
+        if (moneyText != null)
+        {
+            moneyText.text = "Монеты" + money; // обновления текста с количеством монет 
+        }
     }
 }
