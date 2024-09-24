63d9ab28e237c214d2c7ab7efdbc07728a37e45b
Prirogativus <prirogativus@gmail.com>
2024-09-24 17:19:30 +0200

    рандомные пробелы поубирались

--- G2/Assets/Scripts/PortBehaviour.cs
+++ G2/Assets/Scripts/PortBehaviour.cs
@@ -10,22 +10,25 @@ public class PortBehaviour : MonoBehaviour
     public int currentAmountOfSupplies = 1; // Потом поменять на код для корабля
     public int singleSupplyCost = 3;
     public int portSuppliesLevel = 1;
+    public int portProductionLevel = 0;
     private int totalSupplyIncrease = 0;
+    public float productionCooldown = 60f;
+    public int resourseCounter = 0; // Replace later
     private GameManager gameManager;
     void OnShipEntrance()
     {
-       if (isShipInPort)
+        if (isShipInPort)
         {
             GetTotalSupplyIncrease();
             int supplyIncreaseCost = singleSupplyCost * totalSupplyIncrease;
-            if (supplyIncreaseCost < gameManager.money){gameManager.money -= supplyIncreaseCost;}
+            if (supplyIncreaseCost < gameManager.money) { gameManager.money -= supplyIncreaseCost; }
             else { Debug.Log("game over"); }
         }
     }
-    public void GetTotalSupplyIncrease() 
+    public void GetTotalSupplyIncrease()
     {
-       int difference = MaxAmountOfSupplies - currentAmountOfSupplies;
-        if (difference < portSuppliesLevel) 
+        int difference = MaxAmountOfSupplies - currentAmountOfSupplies;
+        if (difference < portSuppliesLevel)
         {
             totalSupplyIncrease = difference;
             currentAmountOfSupplies = MaxAmountOfSupplies;
@@ -45,6 +48,17 @@ public class PortBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        productionCooldown -= Time.deltaTime;
+    }
+    void PortProduction() 
+    {
+        if (portProductionLevel >= 1)
+        {
+           if (productionCooldown < 0) 
+            {
+                resourseCounter +=portProductionLevel;
+                productionCooldown = 60; 
+            }
+        }
     }
 }
