e3ad6cdcffceea8dcf1d5a6754e6702447b3de68
Prirogativus <prirogativus@gmail.com>
2024-09-26 17:19:44 +0200

    кнопка интрефейса
    добавил кноповку которая меняет стандартный интерфейс игры на режим редактирования маршрутов

---
+++ G2/Assets/Scripts/Button Behaviour.cs
@@ -0,0 +1,43 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public GameObject BasicCanvas;
+    public GameObject RoutesEditor;
+    public bool BasicCanvasIsEnabled = true;
+    //public bool RoutesEditorIsEnabled = false;
+    public Image buttonImage;
+    public Sprite enableIcon;
+    public Sprite disableIcon;
+
+    public void ButtonClicked()
+    {
+        BasicCanvasIsEnabled = !BasicCanvasIsEnabled;
+        BasicCanvas.SetActive(BasicCanvasIsEnabled);
+        RoutesEditor.SetActive(!BasicCanvasIsEnabled);
+
+        if (BasicCanvasIsEnabled)
+        {
+            buttonImage.sprite = enableIcon;
+        }
+        else 
+        {
+            buttonImage.sprite = disableIcon;
+        }
+    }
+       
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
