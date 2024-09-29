5b44c12ad79b45d084a127b247567b093834e04e
Prirogativus <prirogativus@gmail.com>
2024-09-29 18:37:44 +0200

    мтьь
    дд

--- G2/Assets/Scripts/CamBehaviour.cs
+++ G2/Assets/Scripts/CamBehaviour.cs
@@ -8,18 +8,7 @@ public class CamBehaviour : MonoBehaviour
      
     private void Update()
     {
-        Vector3 inputDir = new Vector3(0,0,0);
-
-
-        if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
-        if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
-        if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
-        if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
-
-        Vector3 moveDir = transform.forward *inputDir.z + transform.right * inputDir.x;
-
-        float moveSpeed = 300f;
-        transform.position += inputDir*moveSpeed*Time.deltaTime;
+       HandleCameraMovement();
 
         /*float rotateDir = 0f;
         if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
@@ -28,6 +17,25 @@ public class CamBehaviour : MonoBehaviour
 
         transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);*/ // поворот камеры не актуален пока
 
+    }
+    private void HandleCameraMovement()
+    {
+        Vector3 inputDir = new Vector3(0, 0, 0);
+
+
+        if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
+        if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
+        if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
+        if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
+
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+
+        float moveSpeed = 300f;
+        transform.position += inputDir * moveSpeed * Time.deltaTime;
+    }
+    private void HandleCameraZoom() 
+    {
+        
     }
 }
 
