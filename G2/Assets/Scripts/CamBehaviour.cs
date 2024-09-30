ee9e05ab70b413dbcf2cb1a09bfdb9721447a409
Prirogativus <prirogativus@gmail.com>
2024-09-30 07:56:49 +0200

    f

--- G2/Assets/Scripts/CamBehaviour.cs
+++ G2/Assets/Scripts/CamBehaviour.cs
@@ -8,7 +8,22 @@ public class CamBehaviour : MonoBehaviour
      
     private void Update()
     {
+<<<<<<< HEAD
        HandleCameraMovement();
+=======
+        Vector3 inputDir = new Vector3(0,0,0);
+
+
+        if (Input.GetKey(KeyCode.W)) inputDir.z = +1f;
+        if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
+        if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
+        if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
+
+        Vector3 moveDir = transform.forward *inputDir.z + transform.right * inputDir.x;
+
+        float moveSpeed = 300f;
+        transform.position += inputDir*moveSpeed*Time.deltaTime;
+>>>>>>> parent of 0f540c5 (РїРёР·РґРµС†)
 
         /*float rotateDir = 0f;
         if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
@@ -18,6 +33,7 @@ public class CamBehaviour : MonoBehaviour
         transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);*/ // поворот камеры не актуален пока
 
     }
+<<<<<<< HEAD
     private void HandleCameraMovement()
     {
         Vector3 inputDir = new Vector3(0, 0, 0);
@@ -37,5 +53,7 @@ public class CamBehaviour : MonoBehaviour
     {
         
     }
+=======
+>>>>>>> parent of 0f540c5 (РїРёР·РґРµС†)
 }
 
