f3852525ec0e239c15f4ea3fc837361e9d075d1b
Sanek55 <cecggcecg@gmail.com>
2025-01-14 20:35:33 +0100

    усовершенствование кода с деньгами
    удаление старого кода с деньгами и добавление сохранение денег при паузе

--- G2/Assets/Scripts/Managers/MoneyManager.cs
+++ G2/Assets/Scripts/Managers/MoneyManager.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moneyManager : MonoBehaviour
+public class MoneyManager : MonoBehaviour
 {
-    public int money = 500;
+    public int money = 200;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("SavedMoney"))
+        {
+            money = PlayerPrefs.GetInt("SavedMoney");
+        }
+    }
 
     public void SpendMoney(int amount)
     {
         if (money >= amount)
         {
             money -= amount;
-        }
-        else
-        {
-            Debug.Log("недостаточно монет!");
+            SaveMoney();
         }
     }
 
-    public bool HasEnoughtMoney(int amount)
+    public void AddMoney(int amount)
     {
-        return money < amount;
+        money += amount;
+        SaveMoney();
     }
 
+    private void SaveMoney()
+    {
+
+        PlayerPrefs.SetInt("SaveMoney", money);
+        PlayerPrefs.Save();
+    }
 }
